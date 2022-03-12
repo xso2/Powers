2b82144b571ec0e2577a91f405cb94be3cf2d879
xso2 <odaken1002@gmail.com>
2022-03-12 23:53:26 +0900

    投稿結果の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-
-  get 'meal_records/index'
-  get 'meal_records/new'
-  get 'meal_records/create'
-  get 'meal_records/show'
-  get 'meal_records/edit'
-  get 'meal_records/update'
-  get 'meal_records/destroy'
   devise_for :users
   root to: 'homes#index'
 
-
+  resources :users, only: :show
   resources :tweets, only: [:index, :new, :create, :show, :destroy] do
     resources :comments, only: [:create, :destroy]
   end
