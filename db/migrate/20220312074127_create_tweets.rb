2468bb2d58f2e240a1c97eb05c4526415d052b0e
xso2 <odaken1002@gmail.com>
2022-03-12 16:44:20 +0900

    ツイートモデルの作成

---
+++ db/migrate/20220312074127_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.text :text,         null:false
+      t.references :user,   null:false, unique: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_062741) do
+ActiveRecord::Schema.define(version: 2022_03_12_074127) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
