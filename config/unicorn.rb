eb3e308dfc9b87c2d40e80f13a812eb166196214
xso2 <odaken1002@gmail.com>
2022-03-12 15:15:59 +0900

    自動デプロイの完了

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,7 +20,7 @@ stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更
 
 #Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
+timeout 60 
 
 #以下は応用的な設定なので説明は割愛
 
