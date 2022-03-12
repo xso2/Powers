9072e16c3fe1d2fc0e485c33e8a714a1986d1c92
xso2 <odaken1002@gmail.com>
2022-03-12 12:37:06 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'Powers'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/xso2/Powers.git'
+set :repo_url,  'git@github.com:xso2/Powers.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
